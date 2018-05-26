2782a43c7c45b5f0b4ec5b3d233d1d33b46aa678
David Zwagemaker <david@zwagemaker.nl>
2018-05-26 11:44:29 +0100

    Rails minimal setup with geocoding

---
+++ db/migrate/20180526103601_create_locations.rb
@@ -0,0 +1,11 @@
+class CreateLocations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :locations do |t|
+      t.string :title
+      t.string :description
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_05_26_103704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
+  end
 
 end
