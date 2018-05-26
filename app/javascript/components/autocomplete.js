ebb12e3a35d31a9a322f122cdb98408b6548bb70
David Zwagemaker <david@zwagemaker.nl>
2018-05-26 17:08:12 +0100

    Added browser key and autocomplete, both working in development

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -1,7 +1,6 @@
 function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
     var locationAddress = document.getElementById('location_address');
-
     if (locationAddress) {
       var autocomplete = new google.maps.places.Autocomplete(locationAddress, { types: [ 'geocode' ] });
       google.maps.event.addDomListener(locationAddress, 'keydown', function(e) {
